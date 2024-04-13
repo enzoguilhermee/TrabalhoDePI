8b82b26985cc9b9897aa381563ccdce154305bb5
Enzo Guilherme <enzomarks77@gmail.com>
2024-04-13 14:14:50 -0300

    Código corrijido

--- codigo.py
+++ codigo.py
@@ -5,8 +5,18 @@ import os
 import numpy as np
 from PIL import Image #Obs: Dizer no relatorio que esta utilizando a biblioteca PIL
 
+def InicializarMatriz(altura,largura):
+    matriz = []
+    for y in range(altura):
+        linha = []
+        for x in range(largura):
+            linha.append(str(0))
+        matriz.append(linha)
 
+    return matriz
 
+# Necessario para encontrar a mediana em uma lista
+# Pegando o numero mais repetido, é possivel encontrar a mediana
 def Numero_Mais_Repetido(lista):
     numero_1 = 0
     numero_0 = 0
@@ -21,6 +31,8 @@ def Numero_Mais_Repetido(lista):
     else:
         return str(1)
 
+# A biblioteca "PIL", tem a função "open" que ler a imagem como True e False
+# Mas para consertar e virar uma matriz binaria, usa-se essa função
 def ajustar_IMG(matriz, nome_arquivo):
     altura = len(matriz)
     largura = len(matriz[0])
@@ -47,6 +59,7 @@ def ajustar_IMG(matriz, nome_arquivo):
     
     return nova_matriz
 
+# Função necessaria para criar uma nova imagem a partir de uma matriz.
 def matriz_para_pbm(matriz, nome_arquivo):
     altura = len(matriz)
     largura = len(matriz[0])
@@ -62,7 +75,8 @@ def matriz_para_pbm(matriz, nome_arquivo):
                 arquivo.write(str(pixel) + "")
             arquivo.write("\n")
 
-
+# Função necessaria para remover os ruidos
+# OBS: Demora uns 30 segundos...
 def filtro_mediana(matriz, tamanhoDoFiltro):
     altura = len(matriz)
     largura = len(matriz[0])
@@ -75,14 +89,14 @@ def filtro_mediana(matriz, tamanhoDoFiltro):
             for j in range(-(tamanhoDoFiltro // 2), tamanhoDoFiltro // 2 + 1):
                 for i in range(-(tamanhoDoFiltro // 2), tamanhoDoFiltro // 2 + 1):
                     if 0 <= y + j < altura and 0 <= x + i < largura:
-                        print("Matriz y+j: ",y+j, " x+i: ", x+i, " ", matriz[y+j][x+i] )
                         vizinhos.append(matriz[y + j][x + i])
-            print(Numero_Mais_Repetido(vizinhos))
             nova_linha.append(Numero_Mais_Repetido(vizinhos))
         nova_matriz.append(nova_linha)
     
     return nova_matriz
 
+# Função para dilatar as letras, a partir de uma mascara
+# Obs: A mascara deve ser quadrada e com numeros primos, ou seja, 3x3, 5x5, 7x7
 def dilatar(matriz, matrizTransformacao, tamanhoTransformador):
     altura = len(matriz)
     largura = len(matriz[0])
@@ -99,7 +113,7 @@ def dilatar(matriz, matrizTransformacao, tamanhoTransformador):
                     break
                 for i in range(-(tamanhoTransformador // 2), tamanhoTransformador // 2 + 1):
                     if 0 <= y + j < altura and 0 <= x + i < largura:
-                        if matriz[y+j][x+i] == str(matrizTransformacao[j+ajuste][i+ajuste]):
+                        if (matriz[y+j][x+i] == str(matrizTransformacao[j+ajuste][i+ajuste])) and (str(matrizTransformacao[j+ajuste][i+ajuste]) == str(1)):
                             cont = 1
                             break
             if cont == 1:
@@ -110,6 +124,8 @@ def dilatar(matriz, matrizTransformacao, tamanhoTransformador):
     
     return nova_matriz
 
+# Função para erodir as letras, a partir de uma mascara
+# Obs: A mascara deve ser quadrada e com numeros primos, ou seja, 3x3, 5x5, 7x7
 def erosao(matriz, matrizTransformacao, tamanhoTransformador):
     altura = len(matriz)
     largura = len(matriz[0])
@@ -145,7 +161,7 @@ def erosao(matriz, matrizTransformacao, tamanhoTransformador):
     return nova_matriz
 
                     
-
+# Ler uma imagem pre-processada e coloca numa matriz
 def ler_imagem_pbm(nome_arquivo):
     matriz = []
     with open(nome_arquivo, 'r') as arquivo:
@@ -163,190 +179,174 @@ def ler_imagem_pbm(nome_arquivo):
     
     return matriz
 
-matrizTrans =  [[1,1,1,1,1,1,1,1,1,1,1,1,1],
-                [1,1,1,1,1,1,1,1,1,1,1,1,1],
-                [1,1,1,1,1,1,1,1,1,1,1,1,1],
-                [1,1,1,1,1,1,1,1,1,1,1,1,1],
-                [1,1,1,1,1,1,1,1,1,1,1,1,1],
-                [1,1,1,1,1,1,1,1,1,1,1,1,1],
-                [1,1,1,1,1,1,1,1,1,1,1,1,1],
-                [1,1,1,1,1,1,1,1,1,1,1,1,1],
-                [1,1,1,1,1,1,1,1,1,1,1,1,1],
-                [1,1,1,1,1,1,1,1,1,1,1,1,1],
-                [1,1,1,1,1,1,1,1,1,1,1,1,1],
-                [1,1,1,1,1,1,1,1,1,1,1,1,1],
-                [1,1,1,1,1,1,1,1,1,1,1,1,1]
+# Função para contar as palavras
+
+def contar_palavras(matriz, matrizContador):
+    altura = len(matriz)
+    largura = len(matriz[0])
+    #Todas as imagens começa na altura = 179
+    y = 179
+    linha = 0
+    palavras = 0
+
+    dentro_dePalavra = 0
+    mesmaLinha = 0
+
+    paragrafo = 1
+
+    # Enquanto a altura atual não for superior a altura maxima, continue
+    while (y < altura):
+        dentro_dePalavra = 0
+        paragrafo = paragrafo + 1
+        for x in range(largura):
+            cont = 0
+            # Cada iteração é passada uma mascara de 17x1
+            for j in range(-9,9):
+                if 0 <= y+j < altura:
+                    # Se encontrar alguma parte preta, pare, pois encontrou alguma palavra, ou ta dentro de uma
+                    if matriz[y+j][x] == str(matrizContador[j+9][0]):
+                        break
+                    else:
+                        cont = cont + 1
+            # Se pelomenos encontrar uma bit preto, ainda estará dentro de uma palavra
+            if cont != 18 and dentro_dePalavra != 1 and y != altura-3 :
+                if (mesmaLinha == 0):
+                    mesmaLinha = 1
+                    linha = linha + 1
+                palavras = palavras + 1
+                dentro_dePalavra = 1
+            # Se todas os bit forem branco, nao esta dentro da palavra
+            if cont >= 18:
+                dentro_dePalavra = 0
+        # Cada linha tem uma altura alternada, fiz isso para na doidice, mas funcionar kk
+        if paragrafo >= 2:
+            mesmaLinha = 0
+            y = y + 39
+            paragrafo = 0
+        else:
+            mesmaLinha = 0
+            paragrafo = paragrafo + 1
+            y = y + 38
+    
+    return palavras
+
+def contar_palavrasA(matriz, matrizContador):
+    altura = len(matriz)
+    largura = len(matriz[0])
+    matriz_Palavra_Virar_quadrado = InicializarMatriz(altura,largura)
+    #Todas as imagens começa na altura = 179
+    y = 179
+    palavras = 0
+    dentro_dePalavra = 0
+    paragrafo = 1
+
+    # Enquanto a altura atual não for superior a altura maxima, continue
+    while (y < altura):
+        dentro_dePalavra = 0
+        paragrafo = paragrafo + 1
+        for x in range(largura):
+            cont = 0
+            # Cada iteração é passada uma mascara de 17x1
+            for j in range(-9,9):
+                if 0 <= y+j < altura:
+                    # Se encontrar alguma parte preta, pare, pois encontrou alguma palavra, ou ta dentro de uma
+                    if matriz[y+j][x] == str(matrizContador[j+9][0]):
+                        break
+                    else:
+                        cont = cont + 1
+            # Se pelomenos encontrar uma bit preto, ainda estará dentro de uma palavra
+            if cont != 18 and dentro_dePalavra != 1 and y != altura-3 :
+                matriz_Palavra_Virar_quadrado[y][x] = str(1)
+                dentro_dePalavra = 1
+            # Se todas os bit forem branco, nao esta dentro da palavra
+            if cont >= 18:
+                dentro_dePalavra = 0
+        # Cada linha tem uma altura alternada, fiz isso para na doidice, mas funcionar kk
+        if paragrafo >= 2:
+            y = y + 39
+            paragrafo = 0
+        else:
+            paragrafo = paragrafo + 1
+            y = y + 38
+    
+    matriz_Dilatada = dilatar(matriz_Palavra_Virar_quadrado,matrizB,5)
+    matriz_para_pbm(matriz_Dilatada, "imagem2Palavras.pbm")
+
+    return palavras
+
+def contar_coluna(matriz, matrizContador):
+    altura = len(matriz)
+    largura = len(matriz[0])
+    coluna = 0
+    mesmaColuna = 1
+    espaco = 0
+    y = 179
+    for x in range(largura):
+        cont = 0
+        for j in range(-9,9):
+            if matriz[y+j][x] == str(matrizContador[j+9][0]):
+                break
+            else:
+                cont = cont + 1
+        if cont != 18:
+            mesmaColuna = 0
+            espaco = 0
+        else:
+            if cont >= 18:
+                espaco = espaco + 1
+                if (espaco >= 34 and espaco <= 36) and mesmaColuna == 0:
+                    print("Matriz: ", y+3, x)
+                    coluna = coluna + 1
+                    mesmaColuna = 1
+    
+    return coluna
+
+
+
+# Mascara 17x1 para passar em cada palavra
+contador = [[1],[1],[1],[1],[1],[1],[1],[1],[1],[1],[1],[1],[1],[1],[1],[1],[1],[1]]
+
+# Mascara 13x13, usada para dilatar e erodir
+matrizB =  [    [1,1,1,1,1,1,1,1,1,1,0,1,1],
+                [1,1,1,1,1,1,1,1,1,1,0,1,1],
+                [1,1,1,1,1,1,1,1,1,1,0,1,1],
+                [1,1,1,1,1,1,1,1,1,1,0,1,1],
+                [1,1,1,1,1,1,1,1,1,1,0,1,1],
+                [1,1,1,1,1,1,1,1,1,1,0,1,1],
+                [1,1,1,1,1,1,1,1,1,1,0,1,1],
+                [1,1,1,1,1,1,1,1,1,1,0,1,1],
+                [1,1,1,1,1,1,1,1,1,1,0,1,1],
+                [1,1,1,1,1,1,1,1,1,1,0,1,1],
+                [1,1,1,1,1,1,1,1,1,1,0,1,1],
+                [1,1,1,1,1,1,1,1,1,1,0,1,1],
+                [1,1,1,1,1,1,1,1,1,1,0,1,1]
                 ]
 
-matrizTeste = [ ['1','1','1','0','0','0','0','0','0'],
-                ['1','1','1','0','0','0','0','0','0'],
-                ['1','1','1','0','0','0','0','0','0'],
-                ['1','1','1','0','0','0','0','0','0'],
-                ['1','1','1','0','0','0','0','0','0'],
-                ['1','1','1','0','1','0','1','0','1'],
-                ['1','1','1','1','1','1','1','1','1'],
-                ['1','1','1','1','1','1','1','1','1'],
-                ['1','1','1','1','1','1','1','1','1']]
 
 # Exemplo de uso
-nome_arquivo = 'imagemComRuidos.pbm'
-nome_arquivo_corrigido = 'imagemComRuidosCorrigido.pbm'
-nome_arquivo_filtrado = 'imagemSemRuidos.pbm'
-nome_arquivo_dilatado = 'imagemDilatado9x9.pbm'
+i = 3
+nome_arquivo = (f'Imagem{i}.pbm')
+nome_arquivo_corrigido = (f'Imagem{i}Corrigido.pbm')
+#
 
-### Ler a imagem corrigida
+### Ler a imagem com "open"
 img = Image.open(nome_arquivo)
-### transformar em um array
+### transformar em um array de True e False
 matriz = np.array(img)
 ### Trocar os True e False por 0 e 1
-matriz_ajustada = ajustar_IMG(matriz, nome_arquivo_corrigido)
+#matriz_ajustada = ajustar_IMG(matriz, nome_arquivo_corrigido)
 
-matrizA = dilatar(matriz_ajustada, matrizTrans, 3)
-matrizB = erosao(matrizA, matrizTrans, 3)
-matrizC = dilatar(matrizB, matrizTrans, 9)
+# Aplica o filtro da mediana
+### Tempo de 30 segundos ou menos
+#matrizA = filtro_mediana(matriz_ajustada,3)
 
-matriz_para_pbm(matrizC, nome_arquivo_dilatado)
+### Aplica a dilatação com mascara da MatrizB, 11x11
+#matrizC = dilatar(matrizA, matrizB, 11)
+#matriz_para_pbm(matrizC, "Imagem1Dilatada11x11.pbm")
+#matriz_para_pbm(matrizC, f"Imagem{i}Dilatada11x11.pbm")
+matrizC = ler_imagem_pbm(f'Imagem{i}Dilatada11x11.pbm')
+print(contar_palavras(matrizC, contador))
+print(contar_coluna(matrizC, contador))
 
-def letra_para_ponto(nome_arquivo_letra):
-    # Ler a imagem com a letra
-    img_letra = Image.open(nome_arquivo_letra)
-    
-    # Converter para tons de cinza, se necessário
-    if img_letra.mode != 'L':
-        img_letra = img_letra.convert('L')
-    
-    # Aplicar um limiar para binarizar a imagem
-    limite = 128
-    img_binaria = img_letra.point(lambda p: p > limite and 255)
-    
-    # Encontrar o centroide da região branca
-    matriz_binaria = np.array(img_binaria)
-    altura, largura = matriz_binaria.shape
-    soma_x = 0
-    soma_y = 0
-    total_pixels = 0
-    
-    for y in range(altura):
-        for x in range(largura):
-            if matriz_binaria[y][x] == 255:  # Pixel branco
-                soma_x += x
-                soma_y += y
-                total_pixels += 1
-    
-    # Calcular as coordenadas do centroide
-    if total_pixels > 0:
-        centroide_x = soma_x / total_pixels
-        centroide_y = soma_y / total_pixels
-        return (centroide_x, centroide_y)
-    else:
-        return None
-
-#Chamamos essa função passando o nome do arquivo da imagem da letra, e ela retornará as coordenadas do ponto representativo da letra.
-
-#EXEMPLO: 
-#
-'''
-nome_arquivo_letra = 'letraA.png'  # Substitua pelo nome do arquivo da sua imagem com a letra
-ponto_letra = letra_para_ponto(nome_arquivo_letra)
-if ponto_letra is not None:
-    print("Coordenadas do ponto representativo da letra:", ponto_letra)
-else:
-    print("Não foi possível encontrar o ponto representativo da letra.")
-'''
-#  Essa função assume que a letra é branca e o fundo é preto na imagem binarizada. Se a imagem estiver ao contrário, você pode ajustar o limiar ou inverter as cores antes de chamar a função.
-
-
-'''
-Para implementar a compressão de imagens usando a transformada de blocos e identificar linhas e palavras, você pode seguir os seguintes passos:
-
-Dividir a imagem em pequenos blocos não sobrepostos de mesmo tamanho (por exemplo, 8 × 8).
-Aplicar uma transformada 2-D a cada bloco (por exemplo, transformada discreta de Fourier ou transformada de Walsh-Hadamard).
-Identificar linhas de texto analisando os coeficientes transformados para cada bloco.
-Usar os espaçamentos entre dois pontos para definir a distância entre palavras.
-Determinar se há uma ou duas colunas de texto com base na distribuição dos espaçamentos entre palavras.
-Aqui está uma implementação básica desses passos:
-'''
-
-import math
-
-def fft2(bloco):
-    """Calcula a transformada 2D de Fourier de um bloco."""
-    altura, largura = len(bloco), len(bloco[0])
-    coeficientes = [[0j] * largura for _ in range(altura)]
-
-    # Loop para cada coeficiente na transformada
-    for v in range(altura):
-        for u in range(largura):
-            somatorio = 0
-            # Loop para cada pixel no bloco
-            for y in range(altura):
-                for x in range(largura):
-                    # Aplicação da fórmula da transformada de Fourier
-                    somatorio += bloco[y][x] * complex(math.cos(-2 * math.pi * ((u * x / largura) + (v * y / altura))),
-                                                        math.sin(-2 * math.pi * ((u * x / largura) + (v * y / altura))))
-            coeficientes[v][u] = somatorio
-
-    return coeficientes
-
-def encontrar_palavras(coeficientes):
-    """Identifica os espaçamentos entre palavras com base nos coeficientes."""
-    # Implementação necessária com base na análise dos coeficientes
-    pass
-
-def encontrar_numero_colunas(coeficientes):
-    """Determina o número de colunas com base nos coeficientes."""
-    # Implementação necessária com base na análise dos coeficientes
-    pass
-
-def compressao_transformada_blocos(imagem, tamanho_bloco=8):
-    """Comprime a imagem dividindo-a em blocos e aplicando a transformada 2D."""
-    altura, largura = len(imagem), len(imagem[0])
-    coeficientes_comprimidos = []
-
-    # Passo 1: Dividir a imagem em blocos
-    for y in range(0, altura, tamanho_bloco):
-        for x in range(0, largura, tamanho_bloco):
-            bloco = [linha[x:x+tamanho_bloco] for linha in imagem[y:y+tamanho_bloco]]
-
-            # Passo 2: Aplicar a transformada 2-D
-            coeficientes = fft2(bloco)
-
-            # Passo 3: Identificar linhas de texto
-            soma_coeficientes = sum(abs(coef) for linha in coeficientes for coef in linha)
-            if soma_coeficientes > limiar_linha:
-                linha = {'bloco': bloco, 'coeficientes': coeficientes}
-                coeficientes_comprimidos.append(linha)
-
-    # Passo 4: Identificar espaçamentos entre palavras
-    for linha in coeficientes_comprimidos:
-        encontrar_palavras(linha['coeficientes'])
-
-    # Passo 5: Determinar o número de colunas
-    numero_colunas = encontrar_numero_colunas(coeficientes_comprimidos[0]['coeficientes'])
-
-    return coeficientes_comprimidos, numero_colunas
-
-# Exemplo de uso
-'''
-imagem = [[0, 0, 0, 0, 0, 0, 0, 0],
-          [0, 1, 1, 1, 1, 1, 1, 0],
-          [0, 1, 1, 1, 1, 1, 1, 0],
-          [0, 1, 1, 0, 0, 0, 0, 0],
-          [0, 1, 1, 0, 0, 0, 0, 0],
-          [0, 1, 1, 1, 1, 1, 1, 0],
-          [0, 0, 0, 0, 0, 0, 0, 0]]
-'''
-limiar_linha = 10  # Ajuste o limiar conforme necessário
-
-# Comprimir a imagem usando a transformada de blocos
-coeficientes_comprimidos, numero_colunas = compressao_transformada_blocos(imagem)
-
-# Visualizar o resultado
-print("Número de colunas:", numero_colunas)
-
-'''
-Nesta implementação, a função fft2d calcula a transformada 2D de Fourier de um bloco utilizando apenas operações matemáticas básicas. Em seguida, a função compressao_transformada_blocos divide a imagem em blocos, calcula a transformada de Fourier para cada bloco e identifica linhas de texto com base na soma dos coeficientes transformados.
-
-
-Neste código, adicionei duas funções novas: encontrar_palavras e encontrar_numero_colunas. Essas funções serão responsáveis por implementar os passos 4 e 5, respectivamente. No exemplo de uso, após chamar a função compressao_transformada_blocos, o número de colunas é exibido como resultado. Você precisará implementar o corpo dessas funções com base na análise dos coeficientes da transformada de blocos para identificar os espaçamentos entre palavras e determinar o número de colunas.
-'''
+# Para que cada palavra vire um quadrado, utilize essa função contar_palavras:
+# É demorada, mas espere kkk
